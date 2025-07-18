1d08d467ef4f7f008ccd89d1af43fd3c34352bf1
Dhruv Jain <alsomedroo@Dhruvs-MacBook-Air.local>
2025-07-18 17:16:05 +0530

    search bar remove

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function HomePage() {
              <span className="text-[#FFE158]">PASS</span> Karo,<span className="text-[#FFE158]"> EARN</span> Karo <br />
           </h1>
           <div className="flex items-center mt-1">
-            <div className="bg-[#faf7ed] rounded-full flex items-center px-5 py-[10px] w-full max-w-xs shadow-md">
+            {/* <div className="bg-[#faf7ed] rounded-full flex items-center px-5 py-[10px] w-full max-w-xs shadow-md">
               <svg className="w-5 h-5 text-[#5B3DF6] mr-2" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24">
                 <circle cx="11" cy="11" r="8" />
                 <line x1="21" y1="21" x2="16.65" y2="16.65" />
@@ -60,7 +60,7 @@ export default function HomePage() {
                 placeholder="Search listings..."
                 type="text"
               />
-            </div>
+            </div> */}
           </div>
           {/* BUTTON ROW under search bar */}
           <div className="flex gap-4 mt-2">
@@ -80,7 +80,7 @@ export default function HomePage() {
               onClick={() => router.push('/buyer')}
             >
               <Compass size={20} />
-              Browse
+              Browse for items
             </motion.button>
           </div>
         </div>
